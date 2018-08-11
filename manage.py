540e6a38ebf42ebf915d54997fe9e686c912f7bb
wangjinyu124419 <wangjinyu124419@gmail.com>
2018-08-11 18:51:08 +0800

    mysql插件未下载

--- manage.py
+++ manage.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from redis import StrictRedis
+from flask_sqlalchemy import SQLAlchemy
 #创建app实例
 app=Flask(__name__)
 
+db=SQLAlchemy(app)
+
+# app.config['SQLALCHEMY_DATABASE_URI']="mysql://root:mysql@127.0.0.1:3306/flaskdb"
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 #配置app
 class Config(object):
     DEBUG=True
+    SQLALCHEMY_DATABASE_URI="mysql://root:mysql@127.0.0.1:3306/flaskdb_1"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 app.config.from_object(Config)
 
