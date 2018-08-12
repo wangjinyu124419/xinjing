4df533c7e2d0639eba35919b2129523858cf6b45
wangjinyu124419 <wangjinyu124419@gmail.com>
2018-08-12 20:12:13 +0800

    网站title图标　缓存很坑清浏览器　退浏览器

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,10 +1,16 @@
+from flask import current_app
 from flask import render_template
 from flask import session
 from info import myredis
 from . import blue_index
 #创建路由
-@blue_index.route('/',methods=['get','post'])
+@blue_index.route('/')
 def index():
 
-
     return render_template('news/index.html')
+
+
+@blue_index.route('/favicon.ico')
+def favicon():
+    #这里追踪下源码,不太清楚
+    return current_app.send_static_file('news/favicon.ico')
